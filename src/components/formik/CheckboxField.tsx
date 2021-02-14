0a40d3e8e2bbdd6ce5be39c4b58f8baded9aef30
jon.escamilla <jonathanescamilla1@gmail.com>
2021-02-14 14:11:39 -0800

    add generic custom component that uses formik w/ chakra checkbox

---
+++ src/components/formik/CheckboxField.tsx
@@ -0,0 +1,22 @@
+import { FormControl, FormLabel, Checkbox } from '@chakra-ui/react';
+import { FastField, FieldProps } from 'formik';
+import React from 'react';
+import { fieldProps } from './InputField';
+
+export const CheckboxField: React.FC<fieldProps> = ({ name, label }) => {
+  return (
+    <FastField name={name}>
+      {({ meta, field }: FieldProps) => {
+        const isInvalid = !!meta.error && meta.touched;
+        return (
+          <>
+            <FormControl name={name} isInvalid={isInvalid}>
+              {label ? <FormLabel htmlFor={name}>{label}</FormLabel> : null}
+              <Checkbox isInvalid={isInvalid} id={name} {...field} />
+            </FormControl>
+          </>
+        );
+      }}
+    </FastField>
+  );
+};

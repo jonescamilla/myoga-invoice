39e81729a1b5677cad9406233d46898091b28f3f
jon.escamilla <jonathanescamilla1@gmail.com>
2021-02-13 18:45:06 -0800

    add types for invoice

---
+++ src/types.ts
@@ -0,0 +1,61 @@
+export type invoice = {
+  id: string;
+  car: inv_car;
+  customer: inv_customer;
+  ticket: inv_ticket;
+};
+
+export type inv_car = {
+  id: string;
+  year: string;
+  color: string;
+  type: string;
+  make: string;
+  license: string;
+  vin: string;
+  milage: string;
+  engine_size: string;
+  tags: string;
+};
+
+export type inv_customer = {
+  id: string;
+  first_name: string;
+  last_name: string;
+  number: string;
+  number_2: string;
+  address: string;
+  email: string;
+};
+
+export type inv_ticket = {
+  total: string;
+  sub_total: string;
+  service: (inv_part | inv_labor)[];
+  misc_charges: string;
+  save_for_package_job: boolean;
+  tax_exempt: boolean;
+};
+
+type inv_part = {
+  quantity_used: number;
+  total: string;
+  price: string;
+};
+
+type inv_labor = {
+  hours_worked: number;
+  total: string;
+  price: string;
+};
+
+export type client = {
+  id: string;
+  first_name: string;
+  last_name: string;
+  number: string;
+  number_2: string;
+  address: string;
+  email: string;
+  invoices: invoice[];
+};

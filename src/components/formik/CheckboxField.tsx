9df7b3d503e023469d5df4b93c8d777a341d68c5
jon.escamilla <jonathanescamilla1@gmail.com>
2021-02-25 22:40:30 -0800

    git add label dir paramter to change the positioning of the label

--- src/components/formik/CheckboxField.tsx
+++ src/components/formik/CheckboxField.tsx
@@ -1,4 +1,4 @@
-import { FormControl, FormLabel, Checkbox } from '@chakra-ui/react';
+import { FormControl, FormLabel, Checkbox, Flex } from '@chakra-ui/react';
 import { FastField, FieldProps } from 'formik';
 import React from 'react';
 import { fieldProps } from './formikFieldTypes';
@@ -7,7 +7,11 @@ import { fieldProps } from './formikFieldTypes';
  * Custom Formik Field that returns JSX of a Chakra-ui Checkbox w/ predefined styling
  */
 
-export const CheckboxField: React.FC<fieldProps> = ({ name, label }) => {
+export const CheckboxField: React.FC<fieldProps> = ({
+  name,
+  label,
+  labelDir = 'column',
+}) => {
   return (
     <FastField name={name}>
       {({ meta, field }: FieldProps) => {
@@ -15,8 +19,10 @@ export const CheckboxField: React.FC<fieldProps> = ({ name, label }) => {
         return (
           <>
             <FormControl name={name} isInvalid={isInvalid}>
-              {label ? <FormLabel htmlFor={name}>{label}</FormLabel> : null}
-              <Checkbox isInvalid={isInvalid} id={name} {...field} />
+              <Flex flexDir={labelDir} p={1}>
+                {label ? <FormLabel htmlFor={name}>{label}</FormLabel> : null}
+                <Checkbox isInvalid={isInvalid} id={name} {...field} />
+              </Flex>
             </FormControl>
           </>
         );

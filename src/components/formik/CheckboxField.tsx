2cd7ab166acfbeb744cb28ad06a20024fdbf6695
jon.escamilla <jonathanescamilla1@gmail.com>
2021-03-06 09:17:12 -0800

    slight restructuring for improved consistency

--- src/components/formik/CheckboxField.tsx
+++ src/components/formik/CheckboxField.tsx
@@ -7,26 +7,23 @@ import { fieldProps } from './formikFieldTypes';
  * Custom Formik Field that returns JSX of a Chakra-ui Checkbox w/ predefined styling
  */
 
-export const CheckboxField: React.FC<fieldProps> = ({
+export const CheckboxField = ({
   name,
   label,
   labelDir = 'column',
-}) => {
-  return (
-    <FastField name={name}>
-      {({ meta, field }: FieldProps) => {
-        const isInvalid = !!meta.error && meta.touched;
-        return (
-          <>
-            <FormControl name={name} isInvalid={isInvalid}>
-              <Flex flexDir={labelDir} p={1}>
-                {label ? <FormLabel htmlFor={name}>{label}</FormLabel> : null}
-                <Checkbox isInvalid={isInvalid} id={name} {...field} />
-              </Flex>
-            </FormControl>
-          </>
-        );
-      }}
-    </FastField>
-  );
-};
+}: fieldProps) => (
+  <FastField name={name}>
+    {({ meta, field }: FieldProps) => {
+      const isInvalid = !!meta.error && meta.touched;
+
+      return (
+        <FormControl name={name} isInvalid={isInvalid}>
+          <Flex flexDir={labelDir} p={1}>
+            {label ? <FormLabel htmlFor={name}>{label}</FormLabel> : null}
+            <Checkbox isInvalid={isInvalid} id={name} {...field} />
+          </Flex>
+        </FormControl>
+      );
+    }}
+  </FastField>
+);
--- src/components/formik/InputField.tsx
+++ src/components/formik/InputField.tsx
@@ -12,37 +12,32 @@ import { fieldProps } from './formikFieldTypes';
  * Custom Formik Field that returns JSX of a Chakra-ui Input w/ predefined styling
  */
 
-export const InputField: React.FC<fieldProps> = ({
+export const InputField = ({
   name,
   label,
   placeholder,
   labelDir = 'column',
   autoComplete = 'off',
-}) => {
-  return (
-    <>
-      <FastField name={name}>
-        {({ field, meta }: FieldProps) => {
-          const isInvalid = !!meta.error && meta.touched;
-          return (
-            <FormControl name={name} isInvalid={isInvalid}>
-              <Flex flexDir={labelDir} p={1}>
-                {label ? <FormLabel htmlFor={name}>{label}</FormLabel> : null}
-                <Input
-                  autoComplete={autoComplete}
-                  {...field}
-                  id={name}
-                  isInvalid={isInvalid}
-                  name={name}
-                  placeholder={placeholder}
-                  errortext={meta.error}
-                ></Input>
-              </Flex>
-              <FormErrorMessage>{meta.error}</FormErrorMessage>
-            </FormControl>
-          );
-        }}
-      </FastField>
-    </>
-  );
-};
+}: fieldProps) => (
+  <FastField name={name}>
+    {({ field, meta }: FieldProps) => {
+      const isInvalid = !!meta.error && meta.touched;
+
+      return (
+        <FormControl name={name} isInvalid={isInvalid}>
+          <Flex flexDir={labelDir} p={1}>
+            {label ? <FormLabel htmlFor={name}>{label}</FormLabel> : null}
+            <Input
+              {...field}
+              autoComplete={autoComplete}
+              id={name}
+              name={name}
+              placeholder={placeholder}
+            />
+          </Flex>
+          <FormErrorMessage>{meta.error}</FormErrorMessage>
+        </FormControl>
+      );
+    }}
+  </FastField>
+);

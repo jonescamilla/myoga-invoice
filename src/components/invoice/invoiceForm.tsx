6f6560c713fba86034a8e5ae1de6abb35cee56fd
jon.escamilla <jonathanescamilla1@gmail.com>
2021-03-06 09:23:19 -0800

    restructuring and adjusted `Tab` and correseponding `TabPanl` order

--- src/components/invoice/invoiceForm.tsx
+++ src/components/invoice/invoiceForm.tsx
@@ -21,39 +21,38 @@ const InvoiceForm = () => {
   return (
     <Box pt={8} pb={30} mx="auto" height="90vh" maxW="900px">
       <Heading>Invoice</Heading>
-      <Formik initialValues={initialValues} onSubmit={async () => {}}>
+      <Formik
+        initialValues={initialValues}
+        // missing:
+        // validationSchema={}
+        onSubmit={async () => {
+          // missing submit logic
+        }}
+      >
         {({ values }) => (
-          <>
-            <Form>
-              <Tabs
-                isFitted
-                // isManual
-                variant="solid-rounded"
-                colorScheme="gray"
-              >
-                <TabList mb="1em">
-                  <KbdTooltip keys={['alt', 'I']}>
-                    <Tab>Invoice</Tab>
-                  </KbdTooltip>
+          <Form>
+            <Tabs isFitted variant="solid-rounded" colorScheme="gray">
+              <TabList mb="1em">
+                <KbdTooltip keys={['alt', 'I']}>
+                  <Tab>Invoice</Tab>
+                </KbdTooltip>
 
-                  <KbdTooltip keys={['alt', 'C']}>
-                    <Tab>Customer</Tab>
-                  </KbdTooltip>
-                </TabList>
+                <KbdTooltip keys={['alt', 'C']}>
+                  <Tab>Customer</Tab>
+                </KbdTooltip>
+              </TabList>
 
-                <TabPanels>
-                  <TabPanel>
-                    <TicketForm ticket={values.ticket} />
-                  </TabPanel>
+              <TabPanels>
+                <TabPanel>
+                  <CustomerAndVehicleForm />
+                </TabPanel>
 
-                  <TabPanel>
-                    <CustomerAndVehicleForm />
-                  </TabPanel>
-                </TabPanels>
-              </Tabs>
-            </Form>
-            <pre>{JSON.stringify(values, null, 2)}</pre>
-          </>
+                <TabPanel>
+                  <TicketForm ticket={values.ticket} />
+                </TabPanel>
+              </TabPanels>
+            </Tabs>
+          </Form>
         )}
       </Formik>
     </Box>
